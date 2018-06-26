2af1174a038f041e8cb46106a3c01af9c63f0028
JDVelasquez <jdvela158@gmail.com>
2018-06-25 20:44:16 -0600

    cambio loco

--- src/my-view3.js
+++ src/my-view3.js
@@ -24,7 +24,7 @@ class MyView3 extends PolymerElement {
 
       <div class="card">
         <div class="circle">3</div>
-        <h1>View Three</h1>
+        <h1>Vista 3</h1>
         <p>Modus commodo minimum eum te, vero utinam assueverit per eu.</p>
         <p>Ea duis bonorum nec, falli paulo aliquid ei eum.Has at minim mucius aliquam, est id tempor laoreet.Pro saepe pertinax ei, ad pri animal labores suscipiantur.</p>
       </div>
